09bd29abd386d5201cb0fe0cc3db06116a6fb055
Mohammad Hafiz-Al-Masud <45371534+Hafiz-Al-Masud@users.noreply.github.com>
2018-12-19 15:01:27 +0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace OpenXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var doc = WordprocessingDocument.Create(@"D:\myfile.docx", WordprocessingDocumentType.Document))
+            {
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+                mainPart.Document = new DocumentFormat.OpenXml.Wordprocessing.Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = body.AppendChild(new Paragraph());
+                Run run = para.AppendChild(new Run());
+                run.AppendChild(new Text("this is a new document"));
+            }
+        }
+    }
+}
